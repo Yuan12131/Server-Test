81abe805b72dd088cb626cdd6e16967d6eecf65c
leeyuan <dbdks32123@gmail.com>
2023-10-16 13:03:07 +0900

    서버에러로그 함수 선언

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const fs = require('fs'); // fs 불러오기
 
 // 서버 구축
 http.createServer((req, res) => {
+  function serverErrorLog(){
+    res.writeHead(500);
+    return res.end('서버에 문제가 생겼습니다.')
+  }
+  console.log("어떤 요청이 들어오는지 확인", "url -> ", req.url, "method -> ", req.method)
 })
